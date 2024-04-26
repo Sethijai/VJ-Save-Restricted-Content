e5e9b68be3b43a2bfa1483bdc84fb9012a0d2202
Sethijai <148748128+Sethijai@users.noreply.github.com>
2024-04-26 15:03:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -247,34 +247,16 @@ def get_message_type(msg: pyrogram.types.messages_and_media.message.Message):
 	except: pass
 
 
-USAGE = """**FOR PUBLIC CHATS**
+USAGE = """**FOR MORE MATERIAL**
 
-**__just send post/s link__**
+**__https://t.me/OPMASTERLECTURE__**
 
-**FOR PRIVATE CHATS**
+**IIT SCHOOL**
 
-**__first send invite link of the chat (unnecessary if the account of string session already member of the chat)
-then send post/s link__**
+**__https://t.me/IITSCHOOL_OPMASTER__**
 
-**FOR BOT CHATS**
 
-**__send link with** '/b/', **bot's username and message id, you might want to install some unofficial client to get the id like below__**
-
-```
-https://t.me/b/botusername/4321
-```
-
-**MULTI POSTS**
-
-**__send public/private posts link as explained above with formate "from - to" to send multiple messages like below__**
-
-```
-https://t.me/xxxx/1001-1010
-
-https://t.me/c/xxxx/101 - 120
-```
-
-**__note that space in between doesn't matter__**
+**__THIS BOT IS ONLY MADE FOR HACKHEIST__**
 """
 
 
