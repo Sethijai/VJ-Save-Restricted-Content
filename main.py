371c4ed96a881679f5b8e23219a4432d524ffb52
Sethijai <148748128+Sethijai@users.noreply.github.com>
2024-06-10 10:43:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def progress(current, total, message, type):
 # start command
 @bot.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-	bot.send_message(message.chat.id, f"**__👋 Hi** **{message.from_user.mention}**\n\",
+	bot.send_message(message.chat.id, f"",
 	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Update Channel", url="https://t.me/Real_Hackheist")]]), reply_to_message_id=message.id)
 
 
